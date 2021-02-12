ad3efafe4604c1b98f3d01985bc5833b96a1a92f
UmarRoofBundle <umarsyed050897@gmail.com>
2021-02-12 12:17:38 -0600

    Finished with photos api

--- controllers/Album/Album.js
+++ controllers/Album/Album.js
@@ -35,61 +35,6 @@ exports.AddAlbum = async (req, res, next) => {
     next(error);
   }
 };
-//Added Single Photos
-exports.AddAlbumPhoto = async (req, res, next) => {
-  const { id, email, passToken } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-
-    //checking if the email exists
-    if (!user) {
-      return next(new ErrorResponse("Email Invalid", 401));
-    }
-    //checking if the email exists
-    if (user.passToken != passToken) {
-      return next(new ErrorResponse("PassToken Invalid", 401));
-    }
-    //if file is there
-    if (req.files === null) {
-      return res.status(400).json({ msg: "No file found" });
-    }
-
-    const file = req.files.file;
-
-    if (file.mimetype === "image/png" || file.mimetype === "image/jpeg") {
-      const fileName = Date.now() + file.name;
-      file.mv(
-        `${__dirname}../../../../Client/public/uploads/${fileName}`,
-        async (err) => {
-          if (err) {
-            console.log(err);
-            return res.status(500).send(err);
-          }
-          try {
-            const photo = await Photos.create({
-              name: fileName,
-              album: id,
-            });
-
-            await photo.save();
-
-            res.json({
-              fileName: fileName,
-              filePath: `/uploads/${fileName}`,
-            });
-          } catch (err) {
-            next(err);
-          }
-        }
-      );
-    } else {
-      return res.status(400).json({ msg: "Incorrect file type" });
-    }
-  } catch (err) {
-    next(err);
-  }
-};
 //Getting Single Album
 exports.GetAlbums = async (req, res, next) => {
   const { id, email, passToken } = req.body;
@@ -133,10 +78,12 @@ exports.GetAlbumsPhotos = async (req, res, next) => {
     }
 
     const photo = await Photos.find({ album: id });
+    const album = await Album.findById({ _id: id });
 
     res.status(201).json({
       success: true,
       data: photo,
+      album: album,
     });
   } catch (error) {
     next(error);
@@ -162,6 +109,32 @@ exports.DeleteAlbum = async (req, res, next) => {
 
     res.status(201).json({
       success: true,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.GetSinglePhoto = async (req, res, next) => {
+  const { id, email, passToken } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    //checking if the email exists
+    if (!user) {
+      return next(new ErrorResponse("Email Invalid", 401));
+    }
+    //checking if the passToken and email exists
+    if (user.passToken != passToken) {
+      return next(new ErrorResponse("Email/Token Invalid", 401));
+    }
+    //const id = user._id;
+    const photo = await Photos.findById(id);
+
+    res.status(201).json({
+      success: true,
+      data: photo.name,
     });
   } catch (error) {
     next(error);
--- routes/Album/Album.js
+++ routes/Album/Album.js
@@ -1,17 +1,146 @@
 const express = require("express");
 const router = express.Router();
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+require("dotenv").config();
+const crypto = require("crypto");
+
+const User = require("../../models/User");
+const Photos = require("../../models/Photos");
+
 const {
   AddAlbum,
-  AddAlbumPhoto,
   GetAlbums,
   GetAlbumsPhotos,
   DeleteAlbum,
+  GetSinglePhoto,
 } = require("../../controllers/Album/Album");
+const { exit } = require("process");
 
 router.post("/addalbum", AddAlbum);
-router.post("/addalbumphoto", AddAlbumPhoto);
 router.post("/getalbum", GetAlbums);
 router.post("/getalbumphotos", GetAlbumsPhotos);
 router.post("/deletealbum", DeleteAlbum);
+router.post("/getsinglePhoto", GetSinglePhoto);
+
+const storage = multer.memoryStorage({
+  destination: function (req, file, callback) {
+    callback(null, "");
+  },
+});
+//const upload = multer({ storage }).single("image");
+const upload = multer({ storage }).single("image");
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_SECRET_ID,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: "ca-central-1",
+  Acl: "public-read",
+});
+
+router.post("/addalbumphoto", upload, async function (req, res, next) {
+  const { album_id, email, token } = req.headers;
+
+  try {
+    const user = await User.findOne({ email });
+
+    //checking if the email exists
+    if (!user) {
+      return next(new ErrorResponse("Email Invalid", 401));
+    }
+    //checking if the email exists
+    if (user.passToken != token) {
+      return next(new ErrorResponse("PassToken Invalid", 401));
+    }
+    //if file is there
+    if (req.files === null) {
+      return res.status(400).json({ msg: "No file found" });
+    }
+
+    let fileName = req.files.image.name.split(".");
+    const fileType = fileName[fileName.length - 1];
+    const randKey = crypto.randomBytes(10).toString("hex");
+
+    const params = {
+      Bucket: "roofbundlesecondbucket",
+      Key: `${randKey}.${fileType}`,
+      Body: req.files.image.data,
+      Acl: "public-read",
+    };
+
+    s3.upload(params, async (error, data) => {
+      if (error) {
+        res.status(500).send(err);
+      }
+
+      const photo = await Photos.create({
+        name: data.Location,
+        album: album_id,
+      });
+
+      photo.save();
+
+      const photo_len = await Photos.find({
+        album: album_id,
+      }).countDocuments();
+
+      if (photo_len === 25) {
+        photo.delete();
+        return res.status(400).json({ msg: "Too many photos" });
+      }
+
+      res.status(200).json({
+        file: data.Location,
+      });
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/deletealbumphoto", async function (req, res, next) {
+  const { photo_id, email, passToken } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    //checking if the email exists
+    if (!user) {
+      return next(new ErrorResponse("Email Invalid", 401));
+    }
+    //checking if the email exists
+    if (user.passToken != passToken) {
+      return next(new ErrorResponse("PassToken Invalid", 401));
+    }
+
+    const photo = await Photos.findById(photo_id);
+    const newName = photo.name.replace(
+      "https://roofbundlesecondbucket.s3.ca-central-1.amazonaws.com/",
+      ""
+    );
+
+    const params = {
+      Bucket: "roofbundlesecondbucket",
+      Key: newName,
+    };
+    s3.deleteObject(params, async (err, data) => {
+      if (err) {
+        console.log("ERR: ", err);
+        return next(new ErrorResponse("Connection Lost", 500));
+        // an error occurred
+      } else {
+        try {
+          const deletePhoto = await Photos.findByIdAndDelete(photo_id);
+          res.status(201).json({
+            success: true,
+          });
+        } catch (err) {
+          return next(new ErrorResponse("Database Connection Error", 500));
+        }
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
